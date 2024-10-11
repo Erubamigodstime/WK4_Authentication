42d1ed5e8ddbcee36aff4cef547a34c04a1b2626
Erubami <erubamigodstime43@gmail.com>
2024-10-11 17:10:49 +0100

    week 3 created

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const routes = express.Router();
 const user = require('./user');
 const swagger = require('./swagger');
-const temple = require('./temple')
+const temple = require('./temple');
+const  home  = require('./homePage');
 
 routes.use('/', swagger);
 routes.use('/user', user);
 routes.use('/temple', temple);
+routes.use('/', home )
 
 
 
